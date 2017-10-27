ee6845cbff367d9388101ee299ad5a05b8f8490b
wKich <nflazzer@gmail.com>
2017-10-27 18:42:25 +0500

    Prepare to deploy on `now`

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 // @flow
 
-const http = require('http')
-const express = require('express')
-const socket = require('socket.io')
+module.exports = () => {
+  const http = require('http')
+  const express = require('express')
+  const socket = require('socket.io')
 
-const app = express()
-const server = http.Server(app)
-const io = socket(server)
+  const app = express()
+  const server = http.Server(app)
+  const io = socket(server)
 
-const PORT = 3000
-let counter = 0
+  const PORT = 3000
+  let counter = 0
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
-io.on('connection', client => {
-  const interval = setInterval(() => client.emit('counter', { counter }), 1000)
-  console.log('a user connected')
-  client.on('disconnect', () => {
-    clearInterval(interval)
-    console.log('user disconnected')
+  app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+  io.on('connection', client => {
+    const interval = setInterval(() => client.emit('counter', { counter }), 1000)
+    console.log('a user connected')
+    client.on('disconnect', () => {
+      clearInterval(interval)
+      console.log('user disconnected')
+    })
   })
-})
-server.listen(PORT, () => {
-  console.log(`Server listening on *:${PORT}`)
-  setInterval(() => counter += 1, 1000)
-})
+  server.listen(PORT, () => {
+    console.log(`Server listening on *:${PORT}`)
+    setInterval(() => counter += 1, 1000)
+  })
+}
