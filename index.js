917d47f938ef9b4e717c2aa3aa4b1d0830a72fe3
wKich <nflazzer@gmail.com>
2017-10-27 22:33:49 +0500

    Prevent sleep

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
 const http = require('http')
 const express = require('express')
 const socket = require('socket.io')
+const request = require('request-promise')
 
 const app = express()
 const server = http.Server(app)
 const io = socket(server)
 
-let counter = 0
+const startDateTime = Date.now()
 
 app.set('port', process.env.PORT || 5000)
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.get('/_status', (req, res) => res.send(`Uptime: ${(Date.now() - startDateTime) / 1000} seconds`))
+
 io.on('connection', client => {
-  const interval = setInterval(() => client.emit('counter', { counter }), 1000)
-  console.log('a user connected')
-  client.on('disconnect', () => {
-    clearInterval(interval)
-    console.log('user disconnected')
-  })
+  console.log('User connected')
+  client.on('disconnect', () => console.log('User disconnected'))
 })
+
 server.listen(app.get('port'), () => {
   console.log(`Server listening on *:${app.get('port')}`)
-  setInterval(() => counter += 1, 1000)
+  setInterval(() => request.get('https://autoru.herokuapp.com/_status').then(res => console.log(res)), 10 * 60 * 1000)
 })
