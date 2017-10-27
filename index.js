a6e84877347d0df86efdd728f0586f25c52ec8af
wKich <nflazzer@gmail.com>
2017-10-27 21:55:57 +0500

    Use heroku

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 // @flow
 
-module.exports = () => {
-  const http = require('http')
-  const express = require('express')
-  const socket = require('socket.io')
+const http = require('http')
+const express = require('express')
+const socket = require('socket.io')
 
-  const app = express()
-  const server = http.Server(app)
-  const io = socket(server)
+const app = express()
+const server = http.Server(app)
+const io = socket(server)
 
-  const PORT = 3000
-  let counter = 0
+let counter = 0
 
-  app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
-  io.on('connection', client => {
-    const interval = setInterval(() => client.emit('counter', { counter }), 1000)
-    console.log('a user connected')
-    client.on('disconnect', () => {
-      clearInterval(interval)
-      console.log('user disconnected')
-    })
+app.set('port', process.env.PORT || 5000)
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+io.on('connection', client => {
+  const interval = setInterval(() => client.emit('counter', { counter }), 1000)
+  console.log('a user connected')
+  client.on('disconnect', () => {
+    clearInterval(interval)
+    console.log('user disconnected')
   })
-  server.listen(PORT, () => {
-    console.log(`Server listening on *:${PORT}`)
-    setInterval(() => counter += 1, 1000)
-  })
-}
+})
+server.listen(app.get('port'), () => {
+  console.log(`Server listening on *:${app.get('port')}`)
+  setInterval(() => counter += 1, 1000)
+})
