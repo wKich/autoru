9efd5a6bc8a2a1e344d5a650f882a36d4b1eb9f6
wKich <nflazzer@gmail.com>
2017-10-27 18:32:09 +0500

    Add counter to socket

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const server = http.Server(app)
 const io = socket(server)
 
 const PORT = 3000
+let counter = 0
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 io.on('connection', client => {
-  let counter = 0
+  const interval = setInterval(() => client.emit('counter', { counter }), 1000)
   console.log('a user connected')
-  client.on('disconnect', () => console.log('user disconnected'))
-  setInterval(() => client.emit('counter', { counter: counter++ }), 1000)
+  client.on('disconnect', () => {
+    clearInterval(interval)
+    console.log('user disconnected')
+  })
 })
-server.listen(PORT, () => console.log(`Server listening on *:${PORT}`))
+server.listen(PORT, () => {
+  console.log(`Server listening on *:${PORT}`)
+  setInterval(() => counter += 1, 1000)
+})
